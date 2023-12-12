b9fb00192daa8172d456e8391a92897b1af25ae0
Manuel Roldan <manuel.roldan@sima.ai>
2023-12-12 12:42:43 +0100

    Trial message

---
+++ source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'demo'
+copyright = '2023, Manuel Roldan'
+author = 'Manuel Roldan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.video',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+master_doc = 'index'
+html_logo = "_static/logo.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+}
+html_css_files = [
+    'css/css-style.css',
+]
